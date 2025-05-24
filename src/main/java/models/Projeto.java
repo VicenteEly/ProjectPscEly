9dbb681fccdd54ba22977241b150bec3f5c127ef
VicenteEly <vicenteelysobral@gmail.com>
2025-05-23 21:27:43 -0300

    feat: model Projeto

---
+++ src/main/java/models/Projeto.java
@@ -0,0 +1,27 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Pojeto")
+
+public class Projeto {
+    @Id
+    private int id;
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicio;
+    private LocalDate dataPrevistaFim;
+
+    public Projeto(int id, String nome, String descricao, LocalDate dataInicio, LocalDate dataPrevistaFim) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataInicio = dataInicio;
+        this.dataPrevistaFim = dataPrevistaFim;
+    }
+}
